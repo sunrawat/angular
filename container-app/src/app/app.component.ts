da75849df2cd583000f3cb1c38b20ce470eca54c
Suraj Rawat <suraj.rawat@nagarro.com>
2021-08-30 12:04:06 +0530

    udpated.,..

--- container-app/src/app/app.component.ts
+++ container-app/src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'suraj-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements
+OnChanges,
+OnInit,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
   title = 'container-app';
   constructor(){
     console.log('called constructor');
+  }
+
+  ngOnChanges(){
+    console.log("ngOnChanges is called");
+  }
+  ngOnInit(){
+    console.log("ngOnInit is called");
+  }
+  ngDoCheck(){
+    console.log("ngDoCheck is called");
+  }
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit is called");
+  }
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked is called");
+  }
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit is called");
+  }
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked is called");
+  }
+  ngOnDestroy(){
+    console.log("ngOnDestroy is called");
   }
 }
